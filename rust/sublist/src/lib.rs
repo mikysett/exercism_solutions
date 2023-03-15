6a465b5baf2b34bac070c6edfcfd45d637ec9d75
Michele Sessa <mikysett@gmail.com>
2023-03-15 17:05:21 +0000

    rust(sublist): completed exercise

---
+++ rust/sublist/src/lib.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    let sublist = _first_list.is_empty()
+        || _second_list
+            .windows(_first_list.len())
+            .any(|candidate| candidate == _first_list);
+    let superlist = _second_list.is_empty()
+        || _first_list
+            .windows(_second_list.len())
+            .any(|candidate| candidate == _second_list);
+
+    match (sublist, superlist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
+    }
+}
