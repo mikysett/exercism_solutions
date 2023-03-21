3ad9e426f598e065c561c523c9d9b90807fe6391
Michele Sessa <mikysett@gmail.com>
2023-03-21 16:54:11 +0000

    rust(gigasecond): simplify using exponential notation

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,6 @@
-use std::ops::Add;
 use time::{ext::NumericalDuration, PrimitiveDateTime as DateTime};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.add(1_000_000_000.seconds())
+    start + 1e9.seconds()
 }
