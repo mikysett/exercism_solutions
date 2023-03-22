29e6e05c9174ad3d8939d9083d90340ca75c2d13
Michele Sessa <mikysett@gmail.com>
2023-03-22 16:47:26 +0000

    rust(poker): improve winning_cards function
    now more idiomatic

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,6 +1,5 @@
 use std::cmp::Ordering;
 
-use crate::card::*;
 use crate::poker_hand::PokerHand;
 
 pub mod card;
@@ -12,27 +11,19 @@ pub mod poker_hand;
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    let poker_hands: Result<Vec<PokerHand>, <Card as TryFrom<&str>>::Error> = hands
+    let mut poker_hands: Vec<PokerHand> = hands
         .iter()
-        .map(|hand| PokerHand::try_from(*hand))
+        .map(|hand| {
+            PokerHand::try_from(*hand)
+                .unwrap_or_else(|err| panic!("Error at line: {}\n{}", hand, err))
+        })
         .collect();
 
-    let mut result = vec![];
-    match poker_hands {
-        Ok(mut my_hands) => {
-            my_hands.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    poker_hands.sort_by(|a, b| b.partial_cmp(a).unwrap());
 
-            for hand in &my_hands {
-                if hand.partial_cmp(&my_hands[0]).unwrap() == Ordering::Equal {
-                    result.push(hand.str_ref);
-                } else {
-                    break;
-                }
-            }
-        }
-        Err(_) => {
-            panic!("Bad input");
-        }
-    }
-    result
+    poker_hands
+        .iter()
+        .filter(|hand| hand.partial_cmp(&&poker_hands[0]).unwrap() == Ordering::Equal)
+        .map(|hand| hand.str_ref)
+        .collect::<Vec<&str>>()
 }
