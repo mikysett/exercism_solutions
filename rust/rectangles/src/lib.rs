f6605364a4662b12cb0010f4907c2138f746685e
Michele Sessa <mikysett@gmail.com>
2023-03-27 01:37:12 +0100

    rust(rectangles): more functional style

--- rust/rectangles/src/lib.rs
+++ rust/rectangles/src/lib.rs
@@ -1,53 +1,50 @@
 struct Point(usize, usize);
 
-pub fn count(lines: &[&str]) -> u32 {
-    let board_size = Point(
-        lines.first().map(|line| line.len()).unwrap_or(0),
-        lines.len(),
-    );
+pub fn count(board: &[&str]) -> u32 {
+    let board_size = Point(board.first().map(|row| row.len()).unwrap_or(0), board.len());
 
-    lines
+    board
         .iter()
         .enumerate()
-        .map(|(y, line)| {
-            line.chars()
+        .map(|(y, row)| {
+            row.chars()
                 .enumerate()
                 .filter(|(_, c)| *c == '+')
-                .map(|(x, _)| nb_rect_in_line(&Point(x, y), lines, &board_size))
+                .map(|(x, _)| count_rect_from_point(&Point(x, y), board, &board_size))
                 .sum::<u32>()
         })
         .sum()
 }
 
-fn nb_rect_in_line(start: &Point, board: &[&str], board_size: &Point) -> u32 {
-    let mut count = 0;
-    for x in start.0 + 1..board_size.0 {
-        let curr_char = board[start.1].chars().nth(x).unwrap();
-        if curr_char == '+' {
-            count += nb_valid_rect(start, x, board, board_size);
-        } else if curr_char != '-' {
-            break;
-        }
-    }
-    count
+fn count_rect_from_point(start: &Point, board: &[&str], board_size: &Point) -> u32 {
+    board[start.1][start.0 + 1..board_size.0]
+        .chars()
+        .enumerate()
+        .map(|(x, c)| (x + start.0 + 1, c))
+        .take_while(|(_, c)| *c == '-' || *c == '+')
+        .filter(|(_, c)| *c == '+')
+        .fold(0, |acc, (x, _)| {
+            acc + nb_valid_rect(start, x, board, board_size)
+        })
 }
 
 fn nb_valid_rect(start: &Point, last_x: usize, board: &[&str], board_size: &Point) -> u32 {
-    let mut count = 0;
-    for y in start.1 + 1..board_size.1 {
-        let left_char = board[y].chars().nth(start.0).unwrap();
-        let right_char = board[y].chars().nth(last_x).unwrap();
-
-        if left_char == '+' && right_char == '+' {
-            if board[y][start.0..last_x]
-                .chars()
-                .all(|c| c == '-' || c == '+')
-            {
-                count += 1;
-            }
-        } else if left_char == ' ' || left_char == '-' || right_char == ' ' || right_char == '-' {
-            break;
-        }
-    }
-    count
+    board[start.1 + 1..board_size.1]
+        .iter()
+        .map(|row| {
+            (
+                row,
+                row.chars().nth(start.0).unwrap(),
+                row.chars().nth(last_x).unwrap(),
+            )
+        })
+        .take_while(|(_line, left_c, right_c)| {
+            *left_c != ' ' && *left_c != '-' && *right_c != ' ' && *right_c != '-'
+        })
+        .filter(|(row, left_c, right_c)| {
+            *left_c == '+'
+                && *right_c == '+'
+                && row[start.0..last_x].chars().all(|c| c == '-' || c == '+')
+        })
+        .count() as u32
 }
