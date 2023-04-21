21eae54daaf383932357b989d678e9de98c0a90a
Michele Sessa <mikysett@gmail.com>
2023-04-21 02:01:34 +0100

    rust(parallel-letter-frequency): parallel version done with mutexes
    faster than sequential only on large inputs

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,20 +1,41 @@
-use std::sync::mpsc;
-use std::thread;
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let mut freq = HashMap::new();
-    input
-        .iter()
-        .flat_map(|line| {
-            line.chars()
+    let freq = Arc::new(Mutex::new(HashMap::<char, usize>::new()));
+    let mut handles = vec![];
+    let chunk_size = input.len() / worker_count + 1;
+
+    input.chunks(chunk_size).for_each(|chunk| {
+        let chunk = chunk.join("");
+        let freq = Arc::clone(&freq);
+
+        let handle = thread::spawn(move || {
+            let mut line_freq = HashMap::new();
+            chunk
+                .chars()
                 .filter(|c| c.is_alphabetic())
                 .flat_map(|c| c.to_lowercase())
-        })
-        .for_each(|c| {
-            freq.entry(c)
-                .and_modify(|count| *count += 1)
-                .or_insert(1);
+                .for_each(|c| {
+                    line_freq
+                        .entry(c)
+                        .and_modify(|count| *count += 1)
+                        .or_insert(1);
+                });
+
+            let mut freq = freq.lock().unwrap();
+            for (k, v) in line_freq {
+                freq.entry(k).and_modify(|count| *count += v).or_insert(v);
+            }
         });
-    freq
+        handles.push(handle);
+    });
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    let freq = Arc::clone(&freq);
+    let freq = freq.lock().unwrap();
+    freq.clone()
 }
