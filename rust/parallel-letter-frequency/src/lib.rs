e6b0c7fb4dcadfddca0b7b613616f2c210ec3de0
Michele Sessa <mikysett@gmail.com>
2023-04-22 09:55:37 +0100

    rust(parallel-letter-frequency): done without mutexes and arc

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,41 +1,38 @@
+use std::mem;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
 use std::thread;
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let freq = Arc::new(Mutex::new(HashMap::<char, usize>::new()));
-    let mut handles = vec![];
+pub fn frequency<'a>(input: &'a [&str], worker_count: usize) -> HashMap<char, usize> {
+    let mut handles = Vec::with_capacity(worker_count);
     let chunk_size = input.len() / worker_count + 1;
 
     input.chunks(chunk_size).for_each(|chunk| {
         let chunk = chunk.join("");
-        let freq = Arc::clone(&freq);
-
-        let handle = thread::spawn(move || {
-            let mut line_freq = HashMap::new();
-            chunk
-                .chars()
-                .filter(|c| c.is_alphabetic())
-                .flat_map(|c| c.to_lowercase())
-                .for_each(|c| {
-                    line_freq
-                        .entry(c)
-                        .and_modify(|count| *count += 1)
-                        .or_insert(1);
-                });
-
-            let mut freq = freq.lock().unwrap();
-            for (k, v) in line_freq {
-                freq.entry(k).and_modify(|count| *count += v).or_insert(v);
-            }
-        });
+        
+        let handle = thread::spawn(move || counter(chunk));
         handles.push(handle);
     });
 
+    let mut freq = HashMap::new();
     for handle in handles {
-        handle.join().unwrap();
+        handle.join().unwrap().into_iter().for_each(|(k, v)| {
+            *freq.entry(k).or_default() += v;
+        });
     }
-    let freq = Arc::clone(&freq);
-    let freq = freq.lock().unwrap();
-    freq.clone()
+    freq
+}
+
+fn counter(input: String) -> HashMap<char, usize> {
+    let mut line_freq = HashMap::new();
+    input
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .for_each(|c| {
+            line_freq
+                .entry(c)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        });
+    line_freq
 }
