2e1bf5b13db8d81dc385bad50fa42701a6b32a13
Michele Sessa <mikysett@gmail.com>
2023-04-23 16:17:11 +0100

    julia(difference-of-squares): refactor for more idiomatic and clear code

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,20 +1,8 @@
 "Square the sum of the first `n` positive integers"
-function square_of_sum(n)
-    sum = 0
-    for i in 1:n
-        sum += i
-    end
-    sum * sum
-end
+square_of_sum(n) = sum(1:n)^2
 
 "Sum the squares of the first `n` positive integers"
-function sum_of_squares(n)
-    sum = 0;
-    for i in 1:n
-        sum += i * i
-    end
-    sum
-end
+sum_of_squares(n) = sum((1:n).^2)
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
 function difference(n)
