f704ddbbb26f3b4274ae9049458a724545d47a33
Michele Sessa <mikysett@gmail.com>
2023-04-25 13:51:31 +0100

    julia(raindrops): refactor for shorter style

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,15 +1,7 @@
 function raindrops(number)
     result = ""
-    if number % 3 == 0
-        result = "Pling"
-    end
-    if number % 5 == 0
-        result = string(result, "Plang")
-    end
-    if number % 7 == 0
-        result = string(result, "Plong")
-    elseif result == ""
-        result = string(number)
-    end
-    result
+    number % 3 == 0 && (result *= "Pling")
+    number % 5 == 0 && (result *= "Plang")
+    number % 7 == 0 && (result *= "Plong")
+    isempty(result) ? string(number) : result
 end
