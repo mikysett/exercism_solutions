4f1456bea7f4356ab9a67b1d19ca970eeb422645
Michele Sessa <mikysett@gmail.com>
2023-05-22 02:03:11 +0100

    rust(macros): more idiomatic paths using crate keyword

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,15 +1,16 @@
 #[macro_export]
 macro_rules! hashmap {
     () => {{
-        use ::std::collections::HashMap;
-        HashMap::new()
+        crate::HashMap::new()
     }};
 
     ($($k:expr => $v:expr),+ $(,)?) => {{
-        use ::std::collections::HashMap;
+        use crate::HashMap;
         let mut hm = HashMap::new();
 
-        $(hm.insert($k, $v);)+
+        $(
+            hm.insert($k, $v);
+        )+
 
         hm
     }};
