b9f766e96eef676cfdd9943db421b3f40ac7f6b9
Michele Sessa <mikysett@gmail.com>
2023-07-29 13:29:13 +0100

    js(regular-chatbot): exercise done

---
+++ javascript/regular-chatbot/regular-chatbot.js
@@ -0,0 +1,67 @@
+// @ts-check
+
+/**
+ * Given a certain command, help the chatbot recognize whether the command is valid or not.
+ *
+ * @param {string} command
+ * @returns {boolean} whether or not is the command valid
+ */
+
+export function isValidCommand(command) {
+  const regex = /^chatbot/i
+  return regex.test(command)
+}
+
+/**
+ * Given a certain message, help the chatbot get rid of all the emoji's encryption through the message.
+ *
+ * @param {string} message
+ * @returns {string} The message without the emojis encryption
+ */
+export function removeEmoji(message) {
+  const regex = RegExp('emoji[0-9]*', 'gi')
+  return message.replace(regex, '')
+}
+
+/**
+ * Given a certain phone number, help the chatbot recognize whether it is in the correct format.
+ *
+ * @param {string} number
+ * @returns {string} the Chatbot response to the phone Validation
+ */
+export function checkPhoneNumber(number) {
+  const regex = /^\(\+[0-9]{2}\) ([0-9]{3})-([0-9]{3})-([0-9]{3})$/
+
+  if (regex.test(number)) {
+    return "Thanks! You can now download me to your phone."
+  } else {
+    return `Oops, it seems like I can't reach out to ${number}`
+  }
+}
+
+/**
+ * Given a certain response from the user, help the chatbot get only the URL.
+ *
+ * @param {string} userInput
+ * @returns {string[] | null} all the possible URL's that the user may have answered
+ */
+export function getURL(userInput) {
+  const regex = /[\w]+\.[a-z]+/gi
+  return userInput.match(regex)
+}
+
+/**
+ * Greet the user using the full name data from the profile.
+ *
+ * @param {string} fullName
+ * @returns {string} Greeting from the chatbot
+ */
+export function niceToMeetYou(fullName) {
+  const regex = /^(\w+), (\w+)$/i
+  const matches = fullName.match(regex)
+  if (matches !== null) {
+    return `Nice to meet you, ${matches[2]} ${matches[1]}`
+  } else {
+    'Invalid full name'
+  }
+}
