12e5cfa55dd7e6f52fea00c87598e75976d4d60f
Michele Sessa <mikysett@gmail.com>
2023-09-18 20:42:39 +0100

    rust(binary-search): improve solution

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,11 +1,8 @@
 use std::cmp::Ordering;
 
-pub fn find<T>(array: &[T], key: T) -> Option<usize>
-where
-    T: Ord,
-{
+pub fn find<T: Ord>(array: &[T], key: T) -> Option<usize> {
     let half = array.len() / 2;
-    
+
     match array.get(half)?.cmp(&key) {
         Ordering::Equal => Some(half),
         Ordering::Less => find(array.get(half + 1..)?, key).map(|i| i + half + 1),
