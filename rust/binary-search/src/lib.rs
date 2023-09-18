2d8e5e207128c58eda57e12f0ec0515bca518275
Michele Sessa <mikysett@gmail.com>
2023-09-18 20:40:57 +0100

    rust(binary-search): improve solution

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -4,21 +4,11 @@ pub fn find<T>(array: &[T], key: T) -> Option<usize>
 where
     T: Ord,
 {
-    binary_find(array, &key, 0)
-}
-
-fn binary_find<T>(array: &[T], key: &T, offset: usize) -> Option<usize>
-where
-    T: Ord,
-{
-    if array.is_empty() {
-        return None;
-    }
     let half = array.len() / 2;
-
-    match array.get(half)?.cmp(key) {
-        Ordering::Equal => Some(offset + half),
-        Ordering::Less => binary_find(array.get(half + 1..)?, key, offset + half + 1),
-        Ordering::Greater => binary_find(array.get(..half)?, key, offset),
+    
+    match array.get(half)?.cmp(&key) {
+        Ordering::Equal => Some(half),
+        Ordering::Less => find(array.get(half + 1..)?, key).map(|i| i + half + 1),
+        Ordering::Greater => find(array.get(..half)?, key),
     }
 }
