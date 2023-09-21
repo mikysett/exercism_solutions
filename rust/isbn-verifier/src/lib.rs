a2e727e92b5128af5e3c00593de8dc08e1a152f0
Michele Sessa <mikysett@gmail.com>
2023-09-22 00:53:08 +0100

    rust(isbn-verifier): exercise improved

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -1,20 +1,14 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let digits: Vec<u32> = isbn
+    match isbn
         .chars()
         .rev()
         .filter_map(|c| c.to_digit(10).or((c == 'X').then_some(10)))
-        .collect();
-
-    if digits.len() != 10 || digits.iter().skip(1).any(|x| *x == 10) {
-        return false;
-    }
-
-    digits
-        .iter()
         .enumerate()
-        .map(|(i, d)| d * (i as u32 + 1))
-        .sum::<u32>()
-        % 11
-        == 0
+        .filter(|(i, c)| *c != 10 || *i == 0)
+        .fold((0, 0), |a, (i, c)| (a.0 + 1, a.1 + c * (i as u32 + 1)))
+    {
+        (10, sum) => sum % 11 == 0,
+        _ => false,
+    }
 }
