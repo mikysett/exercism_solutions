7cadb9976174cac6b0cf25b0959fc00c59983d37
Michele Sessa <mikysett@gmail.com>
2023-09-22 17:20:52 +0100

    rust(isogram): improve exercise

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -3,14 +3,9 @@ use std::collections::HashSet;
 pub fn check(candidate: &str) -> bool {
     let mut letters = HashSet::new();
 
-    for c in candidate
+    candidate
+        .to_lowercase()
         .chars()
-        .filter_map(|c| c.is_alphabetic().then(|| c.to_ascii_uppercase()))
-    {
-        if letters.contains(&c) {
-            return false;
-        }
-        letters.insert(c);
-    }
-    true
+        .filter(|c| c.is_alphabetic())
+        .all(|c| letters.insert(c))
 }
