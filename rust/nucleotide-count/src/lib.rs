54dae00433df29eac3c08fb0d64b6b4881ab2f5a
Michele Sessa <mikysett@gmail.com>
2023-09-25 12:55:01 +0100

    rust(nucleotide-count): improve exercise

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -8,11 +8,7 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut counts = HashMap::new();
-
-    "ACGT".chars().for_each(|c| {
-        counts.insert(c, 0);
-    });
+    let mut counts: HashMap<_, _> = "ACGT".chars().map(|c| (c, 0)).collect();
 
     dna.chars()
         .try_for_each(|c| match counts.entry(c).and_modify(|e| *e += 1) {
