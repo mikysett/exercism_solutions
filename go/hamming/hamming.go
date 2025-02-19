08ace245770042ade64bc524901ff1acd19e5280
Michele Sessa <michele.sessa@freiehit.com>
2025-02-19 20:40:48 +0000

    solve exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,19 @@
 package hamming
 
+import (
+	"fmt"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("Invalid arguments: can't calculate hamming distance of strings with different length")
+	}
+
+	hammingDistance := 0
+	for i := range a {
+		if a[i] != b[i] {
+			hammingDistance++
+		}
+	}
+	return hammingDistance, nil
 }
