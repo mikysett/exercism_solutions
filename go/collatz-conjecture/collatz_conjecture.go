c1fce46889bac3fcb29f597cabfe392f409d2e48
Michele Sessa <michele.sessa@freiehit.com>
2025-02-19 20:28:49 +0000

    solve exercise

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,22 @@
 package collatzconjecture
 
+import (
+	"fmt"
+)
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, fmt.Errorf("Invalid argument: must be a positive integer > 0.")
+	}
+	return solveCollatz(n, 0)
+}
+
+func solveCollatz(n int, stepsCount int) (int, error) {
+	if n == 1 {
+		return stepsCount, nil
+	}
+	if n%2 == 0 {
+		return solveCollatz(n/2, stepsCount+1)
+	}
+	return solveCollatz(n*3+1, stepsCount+1)
 }
