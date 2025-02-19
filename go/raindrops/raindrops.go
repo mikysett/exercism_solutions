cc28a8c9b799f66847b6afdcf5681ce84b65cd24
Michele Sessa <michele.sessa@freiehit.com>
2025-02-19 20:11:04 +0000

    solve exercise

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,32 @@
 package raindrops
 
+import (
+	"strconv"
+	"strings"
+)
+
+type RainSound struct {
+	number int
+	sound  string
+}
+
+var rainSounds = []RainSound{
+	{number: 3, sound: "Pling"},
+	{number: 5, sound: "Plang"},
+	{number: 7, sound: "Plong"},
+}
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var simphonyBuilder strings.Builder
+	for _, sound := range rainSounds {
+		if number%sound.number == 0 {
+			simphonyBuilder.WriteString(sound.sound)
+		}
+	}
+
+	simphony := simphonyBuilder.String()
+	if len(simphony) == 0 {
+		return strconv.Itoa(number)
+	}
+	return simphony
 }
