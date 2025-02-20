0c6a755ac7ab688a5db066d0f2a36a6981fa79b8
Michele Sessa <michele.sessa@freiehit.com>
2025-02-20 18:39:48 +0100

    simplify recursion function

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -8,15 +8,15 @@ func CollatzConjecture(n int) (int, error) {
 	if n < 1 {
 		return 0, fmt.Errorf("Invalid argument: must be a positive integer > 0.")
 	}
-	return solveCollatz(n, 0)
+	return solveCollatz(n), nil
 }
 
-func solveCollatz(n int, stepsCount int) (int, error) {
+func solveCollatz(n int) int {
 	if n == 1 {
-		return stepsCount, nil
+		return 0
 	}
 	if n%2 == 0 {
-		return solveCollatz(n/2, stepsCount+1)
+		return 1 + solveCollatz(n/2)
 	}
-	return solveCollatz(n*3+1, stepsCount+1)
+	return 1 + solveCollatz(n*3+1)
 }
