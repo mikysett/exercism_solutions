acdc39c8b8dbc8266729c5f28324881eb22dcde0
Michele Sessa <michele.sessa@freiehit.com>
2025-02-20 18:46:41 +0100

    remove useless variable initialization

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,7 +2,7 @@ package scrabble
 
 import "strings"
 
-var letterScores = map[byte]int{
+var letterScores = map[rune]int{
 	// Group with score: 1
 	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
 	// Group with score: 2
@@ -21,10 +21,9 @@ var letterScores = map[byte]int{
 
 func Score(word string) int {
 	totalScore := 0
-	word = strings.ToUpper(word)
 
-	for i := range word {
-		totalScore += letterScores[word[i]]
+	for _, c := range strings.ToUpper(word) {
+		totalScore += letterScores[c]
 	}
 	return totalScore
 }
